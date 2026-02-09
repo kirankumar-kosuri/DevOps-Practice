2fbef73eafdcb0c28614dbbe76fe215fd02d47d2
unknown <kirankumar>
2026-02-09 16:44:51 +0530

    shell

--- 01-conversation.sh
+++ 01-conversation.sh
@@ -10,10 +10,16 @@
 
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 
-echo "$PERSON1 :: Hi $PERSON2, how are you"
-echo "$PERSON2 :: I am fine $PERSON1, what about you"
-echo "$PERSON1 :: What are you learning recently"
-echo "$PERSON2 :: I am learning shell scripting"
+# echo "$PERSON1 :: Hi $PERSON2, how are you"
+# echo "$PERSON2 :: I am fine $PERSON1, what about you"
+# echo "$PERSON1 :: What are you learning recently"
+# echo "$PERSON2 :: I am learning shell scripting"
+
+#!/bin/bash
+
+echo "Please enter your Pin number"
+read PIN_NUMBER
+echo "Pin entered is $PIN_NUMBER"
