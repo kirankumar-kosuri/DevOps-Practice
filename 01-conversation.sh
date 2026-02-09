fa30643c1331a5e95d8be23159583f48fa05b0c7
unknown <kirankumar>
2026-02-09 15:59:40 +0530

    shell

--- 01-conversation.sh
+++ 01-conversation.sh
@@ -1,6 +1,16 @@
+# #!/bin/bash
+
+# echo "kiran :: Hi praveen how are you"
+# echo "praveen :: i am fine praveen, what about you"
+# echo "kiran :: what are you learning recently"
+# echo "praveen :: i am learning shell scripting"
+
 #!/bin/bash
 
-echo "kiran :: Hi praveen how are you"
-echo "praveen :: i am fine praveen, what about you"
-echo "kiran :: what are you learning recently"
-echo "praveen :: i am learning shell scripting"
+PERSON1=Pavan
+PERSON2=Kumar
+
+echo "$PERSON1 :: Hi praveen how are you"
+echo "$PERSON2 :: i am fine PERSON2, what about you $PERSON1"
+echo "$PERSON1 :: what are you learning recently"
+echo "$PERSON2 :: i am learning shell scripting $PERSON1"
