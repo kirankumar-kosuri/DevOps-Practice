41c1a6095a630231b72e404674f8aa83e4f29e9a
unknown <kirankumar>
2026-02-10 18:40:50 +0530

    shell

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-
-# NUMBER=$1
-# if [ $NUMBER -lt 10 ]; then
-#     echo "Given Number $NUMBER is less than 10"
-# elif [ $NUMBER -eq 10 ]; then
-#     echo "Given Number $NUMBER is equal to 10"
-# else
-#     echo "Given Number $NUMBER is greater than 10"
-# fi
-
-echo "Please Enter the Number"
-read NUMBER
-
-if [ $(($NUMBER % 2)) -eq 0 ]; then
-    echo "Given number $NUMBER is EVEN"
+NUMBER1=$1
+if [ $NUMBER -lt 10 ]; then
+    echo "Given Number $NUMBER is Less than 10"
 else
-    echo "Given number $NUMBER is ODD"
+    echo "Given Number $NUMBER is Greater than or Equal to 10"
 fi
