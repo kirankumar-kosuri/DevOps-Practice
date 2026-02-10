61a2dfa5feebceacd331607c100a3896fd98065d
unknown <kirankumar>
2026-02-10 18:46:54 +0530

    shell

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-NUMBER1=$1
+NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
     echo "Given Number $NUMBER is Less than 10"
 else
     echo "Given Number $NUMBER is Greater than or Equal to 10"
-fi
+fi 
