b76f7660e7ace662ea29bc0d858a023a3e1c7737
unknown <kirankumar>
2026-02-10 13:00:43 +0530

    shell

--- 06-installation.sh
+++ 06-installation.sh
@@ -23,7 +23,7 @@ fi
 dnf remove mysql -y
 if [ $? -ne 0 ]; then
     echo "Error :: Installing mysql is failure"
-    exit 1
+    #exit 1
 else
     echo "Installing mysql is success"
 fi
@@ -32,7 +32,7 @@ fi
 dnf remove nginx -y
 if [ $? -ne 0 ]; then
     echo "Error :: Installing nginx is failure"
-    exit 1
+    #exit 1
 else
     echo "Installing nginx is success"
 fi
@@ -41,7 +41,7 @@ fi
 dnf remove python3 -y
 if [ $? -ne 0 ]; then
     echo "Error :: Installing python3 is failure"
-    exit 1
+    #exit 1
 else
     echo "Intalling python3 is success"
 fi
