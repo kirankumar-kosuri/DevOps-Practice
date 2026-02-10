258a8a259c2c9e82afa5a9e08df529dee24bb8b1
unknown <kirankumar>
2026-02-10 12:27:21 +0530

    shell

---
+++ 06-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR :: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
+fi
+
+dnf install mysql -y 
+if [ $? -ne 0 ]; then
+    echo "Error Installing MySQL is Failure"
+else   
+    echo "Installing MySQL is Success"
+fi
