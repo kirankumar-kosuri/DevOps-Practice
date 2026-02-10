dc67a92b500d97307e5f28149b74cf4061654d44
unknown <kirankumar>
2026-02-10 19:06:39 +0530

    shell

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -8,11 +8,11 @@
 #     echo "Given Number $NUMBER is Equal to 10"
 # fi 
 
+
 #!/bin/bash
-echo "Please Enter the Number"
-read NUMBER
-if [ $(($NUMBER % 2)) -eq 0 ]; then
-    echo "Given Number $NUMBER is Even"
+echo "PLEASE ENTER THE NUMBER"
+if [ $(($NUMBER % 2 )) -ne 0 ]; then
+    echo "Given Number $NUMBER is EVEN"
 else
-    echo "Given Number $NUMBER is Odd"
+    echo "Given Number $NUMBER is ODD"
 fi
