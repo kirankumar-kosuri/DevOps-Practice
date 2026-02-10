613dacb4ec8483bfb942d290b2571ec6cd051c85
unknown <kirankumar>
2026-02-10 19:24:16 +0530

    shell

--- 06-installation.sh
+++ 06-installation.sh
@@ -4,12 +4,30 @@ if [ $USERID -ne 0 ]; then
     echo "ERROR :: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
     exit 1
 fi
+#########################################
 
 dnf install mysql -y
 if [ $? -ne 0 ]; then
-    echo "INSTALLING MYSQL IS FAILURE"
+    echo "Installing Mysql is Failure"
     exit 1
 else
-    echo "INSTALLING MYSQL IS SUCCESS"
+    echo "INSTALLING Mysql IS SUCCESS"
 fi  
 
+#########################################
+
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx is Failure"
+else
+    echo "Installing Mysql is Success"
+fi
+
+#########################################
+
+dnf install python3 -y
+if [ $? - ne 0 ]; then
+    echo "Installing Python3 is Failure"
+else
+    echo "Installing Python3 is Success"
+fi
