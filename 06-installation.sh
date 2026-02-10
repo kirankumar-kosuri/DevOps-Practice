4e0a9d47e81920e2d1079598754b8f1c6f2212b9
unknown <kirankumar>
2026-02-10 19:28:21 +0530

    shell

--- 06-installation.sh
+++ 06-installation.sh
@@ -1,33 +1,35 @@
-#!/bin/bash
-USERID=$(id -u)
-if [ $USERID -ne 0 ]; then
-    echo "ERROR :: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
-    exit 1
-fi
-#########################################
+# #!/bin/bash
+# USERID=$(id -u)
+# if [ $USERID -ne 0 ]; then
+#     echo "ERROR :: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
+#     exit 1
+# fi
+# #########################################
 
-dnf install mysql -y
-if [ $? -ne 0 ]; then
-    echo "Installing Mysql is Failure"
-    exit 1
-else
-    echo "Installing Mysql is Success"
-fi  
+# dnf install mysql -y
+# if [ $? -ne 0 ]; then
+#     echo "Installing Mysql is Failure"
+#     exit 1
+# else
+#     echo "Installing Mysql is Success"
+# fi  
 
-#########################################
+# #########################################
 
-dnf install nginx -y
-if [ $? -ne 0 ]; then
-    echo "Installing Nginx is Failure"
-else
-    echo "Installing Nginx is Success"
-fi
+# dnf install nginx -y
+# if [ $? -ne 0 ]; then
+#     echo "Installing Nginx is Failure"
+# else
+#     echo "Installing Nginx is Success"
+# fi
 
-#########################################
+# #########################################
 
-dnf install python3 -y
-if [ $? -ne 0 ]; then
-    echo "Installing Python3 is Failure"
-else
-    echo "Installing Python3 is Success"
-fi
+# dnf install python3 -y
+# if [ $? -ne 0 ]; then
+#     echo "Installing Python3 is Failure"
+# else
+#     echo "Installing Python3 is Success"
+# fi
+
+# #########################################
