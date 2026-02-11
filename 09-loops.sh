ba42164ea2b3e5d1231d3c2c878f4e8faec2e373
unknown <kirankumar>
2026-02-11 11:33:21 +0530

    shell

---
+++ 09-loops.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# for i in {1..10}
+# do
+#     echo $i
+# done
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR :: PLEASE RUN THIS SCRIPT WITH ROOT PRIVILAGE"
+    exit 1
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo "Installing $2 is Failure"
+else
+    echo "Installing $2 is Success"
+fi
+}
+
+for package in $@
+do
+    echo "Package is : $package"
+done
